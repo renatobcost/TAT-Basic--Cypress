574a83ffdc65b2aaf7f1be2aae5bf33a2f54f51b
renatobcost <renatobcosta@alu.ufc.br>
2022-05-26 12:59:02 -0300

    incluir request

---
+++ cypress/integration/CAC-TAT-al12.spec.js
@@ -0,0 +1,20 @@
+/// <reference types="Cypress" />
+
+describe('Central de Atendimento ao Cliente TAT', function() {
+
+    beforeEach(function(){
+        cy.visit('./src/index.html')
+    })
+    
+    it('faz uma requisição http', function() {
+        cy.request('https://cac-tat.s3.eu-central-1.amazonaws.com/index.html')
+          .should( function(response){
+            const {status, statusText, body } = response
+            expect(status).to.equal(200)
+            expect(statusText).to.equal('OK')
+            expect(body).to.include('CAC TAT')
+          })
+    })
+    
+})
+  
